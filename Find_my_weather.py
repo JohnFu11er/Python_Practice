4d2143dad991ecd9d87b673d84d6325bd5aca6fd
JohnFu11er <62799830+JohnFu11er@users.noreply.github.com>
2021-02-07 04:55:10 -0500

    Update Find_my_weather.py

--- Find_my_weather.py
+++ Find_my_weather.py
@@ -1,7 +1,20 @@
+#!/bin/env python3
+
 import json
 import requests
 
-def line_breaks(chars:str):
+'''
+    Filename: Find_my_weather.py
+    Author: John Fuller
+    Date created: 2/7/2021
+    Date last modified: 2/7/2021
+    Description:
+        Provides the weather forecast for a city
+        based on entered latitude and longitude.
+'''
+
+
+def line_breaks(chars: str):
     ''' Returns string with a line break entered at defined interval '''
 
     chars = chars.split(" ")
@@ -14,21 +27,21 @@ def line_breaks(chars:str):
     return output
 
 
-def weather_api_call(lat:int, lon:int):
+def weather_api_call(lat: int, lon: int):
     ''' Returns json dictionary of weather data for assigned lat, lon coordinates '''
-    
+
     # API call to fetch data related to assigned lat, lon coordinates
     api_call = f"https://api.weather.gov/points/{lat},{lon}"
     points_data = requests.get(api_call).json()
-    
+
     who = points_data['properties']['gridId']
     x = points_data['properties']['gridX']
     y = points_data['properties']['gridY']
-    
+
     # API call to fetch weather forecast data
     forecast_api = f"https://api.weather.gov/gridpoints/{who}/{x},{y}/forecast"
     output = requests.get(forecast_api).json()
-    
+
     # Assign extra data to output
     raw_date = output['properties']['updated'][:10].split("-")
     output['date'] = f"{raw_date[1]}/{raw_date[2]}/{raw_date[0]}"
@@ -36,10 +49,12 @@ def weather_api_call(lat:int, lon:int):
     output['state'] = points_data['properties']['relativeLocation']['properties']['state']
     return output
 
+
 def main():
     while True:
         # Coordinates must be in the United States
         # No error checking currently exists for coordinates
+        print("Provide coordinates in decimal notation.  Ex: 37.886, -78.894")
         latitude = float(input("Latitude: "))
         longitude = float(input("Longitude: "))
 
@@ -47,14 +62,16 @@ def main():
         forecast = weather_api_call(latitude, longitude)
 
         # Display weather information for user
-        banner = ("*" * 40 )
+        banner = ("*" * 40)
         print((banner + "\n") * 5)
-        print(f"\nThe daily forecast for {forecast['city']}, {forecast['state']}")
+        print(
+            f"\nThe daily forecast for {forecast['city']}, {forecast['state']}")
         print(f"as of {forecast['date']}\n")
         for period in forecast['properties']['periods']:
-            print( " " * (19 - (int(len(period['name'])/2))), period['name'])
+            print(" " * (19 - (int(len(period['name'])/2))), period['name'])
             print(banner)
             print(line_breaks(period['detailedForecast']) + "\n")
-            
+
+
 if __name__ == "__main__":
     main()
